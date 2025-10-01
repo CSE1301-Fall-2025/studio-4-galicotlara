2f5ce4d19c3cb4c7cc6c251bd7e263287e43b32a
nataliagalicot <g.natalia@wustl.edu>
2025-10-01 15:42:16 -0500

    we made a pretty flag and learned

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,35 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(StdDraw.PINK);
+		StdDraw.filledRectangle(0, 0, 1, 2);
+		StdDraw.setPenColor(StdDraw.CYAN);
+		StdDraw.filledEllipse(0.2, 0.8, 0.08, 0.08);
+		StdDraw.filledEllipse(0.8, 0.8, 0.08, 0.08);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledEllipse(0.23, 0.78, 0.04, 0.04);
+		StdDraw.filledEllipse(0.77, 0.78, 0.04, 0.04);
+
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledEllipse(0.5, 0.3, 0.23, 0.09);
+
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledEllipse(0.5, 0.3, 0.2, 0.06);
+
+		StdDraw.setPenColor(StdDraw.BLUE);
+		//StdDraw.filledEllipse(0.5, 0.3, 0.1, 0.04);
+		StdDraw.filledRectangle(0.5, 0.2, 0.1, 0.1);
+		StdDraw.filledEllipse(0.5, 0.1, 0.1, 0.04);
+
+		StdDraw.setPenColor(StdDraw.GRAY);
+		StdDraw.filledRectangle(0.2, 0.95, 0.12, 0.04);
+		StdDraw.filledRectangle(0.8, 0.95, 0.12, 0.04);
+
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+		StdDraw.filledEllipse(0.5, 0.55, 0.13, 0.06);
+
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledEllipse(0.45, 0.535, 0.015, 0.032);
+		StdDraw.filledEllipse(0.55, 0.535, 0.015, 0.032);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,31 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		System.out.println(shape);
+		int redC = in.nextInt();
+		int greenC = in.nextInt();
+		int blueC = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
+		StdDraw.setPenColor(redC, greenC, blueC);
+		StdDraw.setPenRadius(0.05);
+
+		if (shape.equals("rectangle")  && filled == true){
+			StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		} else if (shape.equals("ellipse") && filled == true){
+			StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+		} else if (shape.equals("rectangle") && filled == false){
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		} else if (shape.equals("ellipse") && filled == false){
+			StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+		} 
+
+	
+	
 	}
 }
